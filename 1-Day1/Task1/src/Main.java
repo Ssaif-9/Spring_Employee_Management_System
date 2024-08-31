5fe7956cd8b0457d4ab4b64a152657762e9182af
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2024-08-31 20:37:10 +0300

    modify Task one

--- 1-Day1/Task1/src/Main.java
+++ 1-Day1/Task1/src/Main.java
@@ -12,6 +12,7 @@ public class Main {
         }
         return min;
     }
+
     public int maxElement(int [] Array )
     {
         int max =Array[0];
@@ -30,9 +31,10 @@ public class Main {
         long startTime = System.nanoTime();
 
         Arrays.sort(Array);
-        for(int numm : Array)
+
+        for(int singleNum : Array)
         {
-            System.out.print(numm);
+            System.out.print(singleNum);
             System.out.print(" ");
         }
         while (first < last) {
@@ -48,34 +50,34 @@ public class Main {
                 last = mid;
             }
         }
-
         long endTime = System.nanoTime();
         long totalTime = endTime - startTime;
         System.out.printf("\nStart Time : %d \n" , startTime);
         System.out.printf("End Time   : %d \n" , endTime);
         System.out.printf("Total Time with nano second  : %d \n" , totalTime);
-        System.out.printf("Total Time with nano second  : %f \n" , (totalTime*Math.pow(10,-9)));
+        System.out.printf("Total Time with  second      : %f \n" , (totalTime*Math.pow(10,-9)));
         return FindIndex;
-
     }
 
-
     public static void main(String[] args) {
-        Main m = new Main();
+
+        int SearchNum = Integer.parseInt(args[0]);
+
+        Main module = new Main();
         Random rand = new Random();
         int [] List =new int[1000];
         for (int i = 0; i < List.length; i++) {
-            List[i]= rand.nextInt(1000);
+            List[i]= rand.nextInt(-500,500);
         }
 
-        System.out.printf("MAXIMam Num in array = %d \n",m.maxElement(List));
-        System.out.printf("MInIMam Num in array = %d \n",m.minElement(List));
-        int FindFlag =m.searchTime(List,12);
+        int FindFlag =module.searchTime(List,SearchNum);
 
         if (FindFlag==-1)
-            System.out.println("Dont Found ");
+            System.out.println("Dont Found ! ");
         else
-            System.out.printf("Find Number at index %d \n",FindFlag);
+            System.out.printf("Find Number %d at index %d \n",SearchNum,FindFlag);
 
+        System.out.printf("MAXIMam Num in array = %d \n",module.maxElement(List));
+        System.out.printf("MInIMam Num in array = %d \n",module.minElement(List));
     }
 }
