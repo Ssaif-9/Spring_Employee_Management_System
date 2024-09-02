7e31a46273251b304f9a041768b063f380119273
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2024-09-02 13:25:31 +0300

    Done Task1 Day 3

--- 3-Day3/3-Task1/src/Main.java
+++ 3-Day3/3-Task1/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws MyException {
+        ThreeMethods TH = new ThreeMethods();
+        try{
+            TH.sendMessage(args[0]);
+            TH.sayMyName(args[1]);
+            TH.setRole(args[2]);
+        } catch (MyException my) {
+            System.out.println(my.getMessage());
+        }
     }
 }
