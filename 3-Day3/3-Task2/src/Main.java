d18555ba8b2fff71de7a04ec421d05133e7f4bc2
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2024-09-03 07:07:33 +0300

    Done Task2 Day 3

--- 3-Day3/3-Task2/src/Main.java
+++ 3-Day3/3-Task2/src/Main.java
@@ -1,19 +1,26 @@
 import java.lang.reflect.Method;
 
 public class Main {
-
-    public static void main(String[] args) {
-
-        Employee Em = new Employee();
+    @Author(name = "Seif Sultan", salary = 25000)
+    public static void myMethod() {
+        Main ob = new Main();
+        // Obtain the annotation for this method and display the
+        // values of the members.
         try {
-            Class<?> c = Em.getClass();
-            Method m = c.getMethod("Mohammed");
-            Author AU = m.getAnnotation(Author.class);
-
-            System.out.println(AU.name() + " " + AU.salary());
-
-        } catch (NoSuchMethodException E) {
-            System.out.println("Method not found.");
+            // First, get a Class object that represents this class.
+            Class<?> c = ob.getClass();
+            // Now, get a Method object that represents this method.
+            Method m = c.getMethod("myMethod");              // Another class ??
+            // Next, get the annotation for this class.
+            Author an =
+                    m.getAnnotation(Author.class);
+            // Finally, display the values.
+            System.out.println("Name is [" +an.name() + "] AND " +"Salary [" + an.salary() + "].\n");
+        } catch (Exception e) {
+            System.out.println("Method	Not	Found.");
         }
     }
-}
+    public static void main(String[] args) {
+        myMethod();
+        }
+    }
