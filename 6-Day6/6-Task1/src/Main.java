a598d56065745abe6ca56080d60c3557c17816d9
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2024-09-05 13:33:14 +0300

    Task1  Day 6

--- 6-Day6/6-Task1/src/Main.java
+++ 6-Day6/6-Task1/src/Main.java
@@ -1,21 +1,42 @@
-import java.sql.*;
+class MyTask1 implements Runnable{
+    @Override
+    public void run(){
+        for (int i = 0 ; i < 5 ; i++ )
+        {
+            System.out.println("Welcome !");
+        }
+    }
+}
+
+class MyTask2 implements Runnable{
+    @Override
+    public void run(){
+        for (int i = 0 ; i < 5 ; i++ )
+        {
+            System.out.println("Hello !");
+        }
+    }
+}
+
 
 public class Main {
+
     public static void main(String[] args) {
-        try{
-            Connection url = DriverManager.getConnection("jdbc:mysql://Localhost:3306/emp");
-            Statement stmt = url.createStatement();
-            String sql =new String( "select * from emp");
-            ResultSet rs = stmt.executeQuery("select * from emp");
-            {
-                while(rs.next()){
-                    System.out.println("ID : "+rs.getInt(1)+"First Name : "+rs.getString(2)+"Last Name : "+rs.getString(3)+"Salary : "+rs.getInt(4));
-                }
-            }
-            stmt.close();
-            url.close();
-        }catch (SQLException e) {
-            e.printStackTrace();
-        }
+
+        MyTask1 task1 = new MyTask1();
+        Thread thread1 = new Thread(task1);
+
+
+        MyTask2 task2 = new MyTask2();
+        Thread thread2 = new Thread(task2);
+
+        thread2.start();
+        thread1.start();
+
+
+        thread2.run();
+        thread1.run();
+
+
     }
 }
