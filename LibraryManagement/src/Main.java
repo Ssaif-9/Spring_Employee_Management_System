7d8a81b2ba5b1b65e30d8e11d5233efd77040b39
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2024-09-05 16:18:52 +0300

    Start Library Management Project

---
+++ LibraryManagement/src/Main.java
@@ -0,0 +1,12 @@
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Book book = new Book(4,"Rust","mohsen","2021");
+        DatabsaseConnection connection = new DatabsaseConnection();
+        //connection.jdbcAddRow(book);
+        connection.jdbcGetRow("java");
+    }
+}
