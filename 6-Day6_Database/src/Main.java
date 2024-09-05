42199f0ee3ab7aef6681d81fb6deaf958f27a771
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2024-09-05 12:05:30 +0300

    Class Work for Database  Day 6

--- 6-Day6_Database/src/Main.java
+++ 6-Day6_Database/src/Main.java
@@ -3,16 +3,22 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) {
         try{
-            Connection url = DriverManager.getConnection("jdbc:mysql://Localhost:3306/emp");
-            Statement stmt = url.createStatement();
-            String sql =new String( "select * from emp");
-            ResultSet rs = stmt.executeQuery("select * from emp");
+            Connection url = DriverManager.getConnection("jdbc:mysql://Localhost:3306/emp","root","root");
+            //Statement stmt = url.createStatement();
+            String sql = "select * from employee where id = ? or first_name = ? ";
+            PreparedStatement pstmt = url.prepareStatement(sql);
+            pstmt.setInt(1,1);
+            pstmt.setString(2,"ahmed");
+            ResultSet rs = pstmt.executeQuery(); //if stmt must send sql to executeQuery
             {
                 while(rs.next()){
-                    System.out.println("ID : "+rs.getInt(1)+"First Name : "+rs.getString(2)+"Last Name : "+rs.getString(3)+"Salary : "+rs.getInt(4));
+                    System.out.println("ID : "+rs.getInt(1)+" "
+                                      +"First Name : " +rs.getString(2)+" "
+                                      +"Last Name : "+rs.getString(3)+" "
+                                      +"Salary : "+rs.getInt(4));
                 }
             }
-            stmt.close();
+            pstmt.close();
             url.close();
         }catch (SQLException e) {
             e.printStackTrace();
