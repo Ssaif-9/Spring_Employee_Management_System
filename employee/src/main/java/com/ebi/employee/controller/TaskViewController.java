367e652ac39a14e16419e68723beaeb52f6e6eff
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2024-09-18 12:06:37 +0300

    Handle all Employee page frontend

--- employee/src/main/java/com/ebi/employee/controller/TaskViewController.java
+++ employee/src/main/java/com/ebi/employee/controller/TaskViewController.java
@@ -1,8 +1,11 @@
 package com.ebi.employee.controller;
 
+import com.ebi.employee.entity.EmployeeEntity;
+import com.ebi.employee.exception.CustomException;
 import com.ebi.employee.model.EmployeeSaveDto;
 import com.ebi.employee.model.GeneralResponse;
 import com.ebi.employee.model.TaskSaveDto;
+import com.ebi.employee.repo.EmployeeRepoInterface;
 import com.ebi.employee.service.TaskServiceInterface;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -10,9 +13,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/task")
@@ -20,6 +25,7 @@ import java.util.List;
 public class TaskViewController {
 
     private final TaskServiceInterface taskServiceInterface;
+    private final EmployeeRepoInterface employeeRepoInterface;
 
     @Value("${Success.Get.code}")
     private String GetCode;
@@ -34,6 +40,21 @@ public class TaskViewController {
         model.addAttribute("response",response);
         return "listTask";
     }
+
+    @PostMapping("/add")
+    public String getAddTask(TaskSaveDto taskSaveDto ,Model model){
+        Optional<EmployeeEntity> employeeSaveDto =employeeRepoInterface.findById(taskSaveDto.getEmployeeId());
+       if(employeeSaveDto.isPresent()){
+            taskServiceInterface.addTask(taskSaveDto);
+            model.addAttribute("task",taskSaveDto);
+            return "addTask";
+        }
+        else
+            throw new CustomException("001","not Exist Employee","Can not find employee who make this task ");
+
+    }
+
+
 
 
 }
