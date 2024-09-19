49afc6cf9b637381c6ed971a6dd23e6b08916445
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2024-09-19 22:52:32 +0300

    Handle Files

--- 3-Day3/3-Task2/src/Author.java
+++
@@ -1,9 +0,0 @@
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.CONSTRUCTOR,ElementType.METHOD,ElementType.PARAMETER,ElementType.TYPE,ElementType.FIELD})
-
-public @interface Author {
-    String name() default "";
-    int salary();
-}
